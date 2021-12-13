0ed3be0681e1616234d3ff8151badd211171fbad
BBScherz <bscherz1@umbc.edu>
2021-12-13 11:23:57 -0500

    Started MaxEnt feature design

---
+++ maxent_main.py
@@ -0,0 +1,47 @@
+import re
+
+
+adjectives = []
+def loadAdj():
+
+    with open("adjectiveList.txt", "r", encoding="utf-8") as fs:
+
+        for line in fs:
+
+            cur = line.split(",")
+            for adj in cur:
+
+                if(adj not in adjectives):
+                    adjectives.append(adj)  
+
+    print("Total Unique Adjectives",len(adjectives))
+
+#feature test sentence. first review in reviews.txt
+tsen = "Stayed in a king suite for 11 nights and yes it cots us a bit but we were happy with the standard of room, the location and the friendliness of the staff. Our room was on the 20th floor overlooking Broadway and the madhouse of the Fairway Market. Room was quite with no noise evident from the hallway or adjoining rooms. It was great to be able to open windows when we craved fresh rather than heated air. The beds, including the fold out sofa bed, were comfortable and the rooms were cleaned well. Wi-fi access worked like a dream with only one connectivity issue on our first night and this was promptly responded to with a call from the service provider to ensure that all was well. The location close to the 72nd Street subway station is great and the complimentary umbrellas on the drizzly days were greatly appreciated. It is fabulous to have the kitchen with cooking facilities and the access to a whole range of fresh foods directly across the road at Fairway.This is the second time that members of the party have stayed at the Beacon and it will certainly be our hotel of choice for future visits."
+
+
+#Features
+def review_len(rvw):
+
+    sen = re.split(" ", rvw)
+    return len(sen)
+
+def adj_count(rvw):
+
+    adjs = 0
+
+    sens = re.split(" ", rvw)
+    for word in sens:
+
+        if(word in adjectives):
+            adjs += 1
+
+    return adjs
+    
+
+
+if(__name__ == "__main__"):
+    loadAdj()
+
+    print("Sentence length: ",review_len(tsen))
+    print("Adjectives found: ",adj_count(tsen))
